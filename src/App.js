345b915a6c552b7e2806a067f29c81168cd3698f
Ricardo <ricardo@Ricardos-Mac-mini.local>
2019-12-25 14:22:32 -0800

    validation completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react'
+import Validation from './validation/Validation.js';
+import Char from './validation/Char/Char.js'
+
 
 class App extends Component {
-  state= [
-    input:''
-  ]
-  
+  state= {
+    userInput: ''
+  }
+
+  inputHandler = (event) => {
+    this.setState({ userInput: event.target.value })
+  }
+
+
   render() {
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return<Char character={ch} key={index}/>
+    })
     return (
       <div>
-        <h1>hello</h1>
+        <h1>Hello</h1>
+        <hr />
+        <input
+          type="text"
+          onChange={this.inputHandler}
+          value={this.state.userInput}
+        />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     )
   }
 }
 
-export default App; 
+export default App;
---
+++ src/validation/Validation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const validation = (props) => {
+    let validationMessage= "text too short"
+
+    if(props.inputLength >= 5) {
+        validationMessage = "text long enough"
+    }
+    return (
+        <div>
+            <p>{validationMessage}</p>
+        </div>
+    )
+}
+
+export default validation 
